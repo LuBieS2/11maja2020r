a5e56dcbaa3d075a7e2a874d51862312feadce3d
LuBieS2 <119162617+LuBieS2@users.noreply.github.com>
2023-10-01 17:08:56 +0200

    tyuu

--- zadania.py
+++ zadania.py
@@ -1,6 +1,6 @@
 #https://cke.gov.pl/images/_EGZAMIN_MATURALNY_OD_2015/Arkusze_egzaminacyjne/2020/formula_od_2015/informatyka/MIN-R2_1P-202.pdf
 from sympy import isprime
-file=open("pary.txt", "r")
+file=open("przyklad.txt", "r")
 pairs=list(map(str.strip, file))
 pairs = [item.split(" ") for item in pairs]
 #4.1
@@ -16,3 +16,22 @@ for i in numbers1:
         if isprime(n) and isprime(int(i)-n):
             print(i, n, int(i)-n)
             break
+#4.2
+words=[]
+for pair in pairs:
+    words.append(pair[1])
+print(words)
+for word in words:
+    counter = 1
+    biggest= 0
+    b=0
+    for i in range(len(word)-1):
+        if word[i]==word[i+1]:
+            counter+=1
+        else:
+            if counter>biggest:
+                biggest=counter
+                b=i-1
+            counter=1
+    print(biggest, word[b])
+#4.3
