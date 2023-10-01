1158c008f812d4784bc06737563495601f03c3a2
LuBieS2 <119162617+LuBieS2@users.noreply.github.com>
2023-10-01 16:29:15 +0200

    tyuu

--- zadania.py
+++ zadania.py
@@ -1,19 +1,18 @@
 #https://cke.gov.pl/images/_EGZAMIN_MATURALNY_OD_2015/Arkusze_egzaminacyjne/2020/formula_od_2015/informatyka/MIN-R2_1P-202.pdf
 from sympy import isprime
-file=open("przyklad.txt", "r")
+file=open("pary.txt", "r")
 pairs=list(map(str.strip, file))
 pairs = [item.split(" ") for item in pairs]
-print(pairs)
+#4.1
 numbers=[]
 for pair in pairs:
     numbers.append(pair[0])
-print(numbers)
 numbers1=[]
 for i in numbers:
     if int (i)>4 and float(i)%2==0:
         numbers1.append(i)
-print(numbers1)
 for i in numbers1:
-    for n in range(i+1):
-        if isprime(n)==True:
+    for n in range(int(i)+1):
+        if isprime(n) and isprime(int(i)-n):
             print(i, n, int(i)-n)
+            break
